d053b765069c1a602bf9513df5b5985ba1fcf82a
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-02-11 23:28:02 -0300

    feat: cria exemplo de xss

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile("/users/pedro/programing/xss/files/index.html", (err) => {
+    console.log("error for send file", err);
+  });
+});
+
+app.get("/response", (req, res) => {
+  res.send(req.query.search);
+});
+
+app.listen(PORT, () => {
+  console.log(`sever init in ${PORT} port`);
+});
